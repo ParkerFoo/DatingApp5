fe349555092c4bb6dbfa68505b97b49673b88634
et3rnal <aaronmaxwell10@pm.me>
2021-07-19 22:02:59 +0800

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; } //EF will be able to indetify this is the PK, just make sure call it as 'Id'. Will auto increment
+        public string UserName { get; set; }
+        
+    }
+}
